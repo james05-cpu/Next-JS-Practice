d6a1eaf1674fd1b01f08172d400397f01637bf7a
MUTUA <99032516+james05-cpu@users.noreply.github.com>
2024-06-20 21:28:21 +0300

    Add files via upload

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div><h1>About us page! </h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium 
+        omnis quibusdam placeat, natus quod, corrupti nam quis blanditiis,
+         fugit velit quam quidem libero eius. Quas, voluptatem quod.
+          Eligendi, nam neque?</p>
+      </div>
+    )
+  }
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <div>
+        <h1> welcome home!</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Ea vel eius porro molestias. 
+            Libero asperiores repellat, aut deleniti maxime eos voluptatibus, 
+            magni ratione repellendus sunt nihil perferendis fugit minus odit!</p>
+        </div>
+    )
+  }
